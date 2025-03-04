562367cfa89cd5a3a10992def9709f9c7ae215c2
harry47 <142928207+hemu0407@users.noreply.github.com>
2025-03-04 12:56:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 import numpy as np
 
 # Alpha Vantage API Key
-API_KEY = "YOUR_ALPHA_VANTAGE_API_KEY"
+API_KEY = "FG9COB3AYV3V9FVK"
 
 # List of companies and their stock symbols
 companies = {
